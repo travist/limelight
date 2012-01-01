a0d85c740c1c09fa18898f09a04af9e405cc2051
Travis Tidwell <travist349@gmail.com>
2012-01-01 07:35:24 +0100

    Added support to update and set custom properties.

--- LimelightResource.php
+++ LimelightResource.php
@@ -10,6 +10,9 @@ class LimelightResource extends restPHP_Resource {
   /** The title of this resource. */
   public $title = NULL;
 
+  /** An array of custom data. */
+  public $custom_property = NULL;
+
   /**
    * Create the server object.
    */
@@ -90,6 +93,40 @@ class LimelightResource extends restPHP_Resource {
 
     // Return the endpoint.
     return $endpoint;
+  }
+
+  /**
+   * Perform a set along with custom data.
+   */
+  public function set($params = array()) {
+
+    // Set the parent first.
+    parent::set($params);
+
+    // See if there is custom data to be set.
+    if (isset($this->diff['custom_property'])) {
+
+      // Set the custom data.
+      $endpoint = $this->endpoint('set');
+      $endpoint .= '/custom';
+      $this->server->setConfig('authenticate', TRUE);
+      $this->server->put($endpoint, $this->diff['custom_property']);
+      $this->server->setConfig('authenticate', FALSE);
+
+      /**
+       * As long as no errors occured, we can assume the call was a success.
+       * Update the custom properties since there isn't any return.
+       */
+      if (!$this->errors()) {
+        foreach ($this->diff['custom_property'] as $key => $value) {
+          if (isset($this->custom_property->{$key})) {
+            $this->custom_property->{$key} = $value;
+          }
+        }
+      }
+    }
+
+    return $this;
   }
 
   /**
